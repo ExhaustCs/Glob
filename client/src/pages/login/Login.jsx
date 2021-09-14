6e360bad5e524a20de5152f12b47a682f72b8ec5
exhaustcs <exhaustcs@gmail.com>
2021-09-13 20:45:07 -0400

    working on implementing context provider

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,25 +1,40 @@
+import { useContext, useRef } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../../context/Context";
 import "./login.css";
 
 export default function Login() {
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const { dispatch, isFetching } = useContext(Context);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+  };
+
   return (
     <div className="login">
-      <form className="loginForm">
+      <form className="loginForm" onSubmit={handleSubmit}>
         <label>Email</label>
+        {/* username */}
         <input
           className="loginInput"
           type="text"
           id=""
-          placeholder="Enter your email..."
+          placeholder="Enter your username..."
+          ref={userRef}
         />
         <label>Password</label>
+        {/* password */}
         <input
           className="loginInput"
           type="password"
           id=""
           placeholder="Enter your password..."
+          ref={passwordRef}
         />
-        <button className="loginButton">
+        <button className="loginButton" type="submit">
           <Link className="link" to="/login">
             Login
           </Link>
